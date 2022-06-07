be3512c5d3773c765c90543679b23b4531565423
Nikolay <popov0772@gmail.ru>
2022-06-07 15:29:43 +0500

    started new file Seminar 1

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ Console.Write("Input integer number: ");
+ int num = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Result is " + num * num);
+*/
+
+/*
+Console.Write("Input first integer number: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second integer number: ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+
+if (num_2 * num_2 == 0)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+*/
+
+Console.Write("Input first integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+while (current <= num)
+{
+    Console.Write(current + " ");
+    current ++;
+}
