3472a5f568f05342c8370be5428b89667b09e361
Nikolay <popov0772@gmail.ru>
2022-06-07 16:59:32 +0500

    was did first task

--- Home_Work/HomeWork001/Program.cs
+++ Home_Work/HomeWork001/Program.cs
@@ -4,3 +4,27 @@
 какое меньшее.  
 */
 
+Console.Write("Введите первое чиcло: ");
+int number_1 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine()); 
+
+if (number_1 > number_2)
+{
+    Console.WriteLine($"Наибольшее число- {number_1} из чисел {number_1} и {number_2}");
+}
+
+if (number_1 == number_2)
+{
+    Console.WriteLine($"Числа {number_1} и {number_2} равны");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число- {number_2} из чисел {number_1} и {number_2}");
+}
+
+
+
+// принимает число в качестве строки из терминала, преобразует его в цельночисленный формат, присваивает 
+// переменной number_1 значение введенного числа.
+// .Parse конвертирует только sting  в любой формат.
