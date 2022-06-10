2457d86f9c20b2c5412ba6cd54d7a8e187310294
Nikolay <popov0772@gmail.ru>
2022-06-10 15:58:47 +0500

    started Home Work 2

---
+++ Home_Work/HomeWork002/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе
+ показывает вторую цифру этого числа.
+
+// попытка работы со string 1 вариант
+
+char ThreeDigitNumberHalf (string threeDigit)
+{
+
+    char half = Convert.ToChar(threeDigit[1]);
+    return half;
+}
+
+// второй вариант
+
+int SecondOfThree (int threeDigit)
+{
+    return threeDigit / 10 % 10;
+}
+*/
