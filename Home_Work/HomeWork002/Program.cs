5bfca4ddbbc047d681216a3fdc59f28248f1a00a
Nikolay <popov0772@gmail.ru>
2022-06-10 22:08:26 +0500

    finished task 2 in two variats

--- Home_Work/HomeWork002/Program.cs
+++ Home_Work/HomeWork002/Program.cs
@@ -1,12 +1,15 @@
-﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе
- показывает вторую цифру этого числа.
+﻿using System;
+/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе
+ показывает вторую цифру этого числа. 
 
 // попытка работы со string 1 вариант
 
 char ThreeDigitNumberHalf (string threeDigit)
 {
-
-    char half = Convert.ToChar(threeDigit[1]);
+    char nul = threeDigit[0];
+    int counter = 0;
+    if (nul == '-') counter ++;
+    char half = threeDigit[1 + counter];
     return half;
 }
 
@@ -14,6 +17,54 @@ char ThreeDigitNumberHalf (string threeDigit)
 
 int SecondOfThree (int threeDigit)
 {
-    return threeDigit / 10 % 10;
+    if (threeDigit < 0) return threeDigit / 10 % 10 * -1;
+    else return threeDigit / 10 % 10;
 }
+
+Задача 13: Напишите программу, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет.
+// 1 решение через строковые методы
+
+string ThirdDigitInNumber (string number)
+{
+    char nul = number[0];
+    int count = 0;
+    int lengthNumber = number.Length;
+
+    if (nul == '-')
+    {
+        count ++;
+    }
+
+    if (lengthNumber < (3 + count))
+        {
+        return "The third Digit is absent";
+        
+        }
+    else 
+        {
+            string third =  "" + number[2 + count];
+            return third;
+        }
+}
+
+Second variant
+
+string ThirdDigitInNumberArithmetic (int number)
+{
+    if (number / 100 != 0)
+    {
+        if (number > 0 ) 
+            {
+                return Convert.ToString (number / 100 % 10);
+            }
+        else return Convert.ToString (number / 100 % 10 * -1);
+    }    
+    else return "The third Digit is absent";
+}
+
 */
+Console.Write("Input number: ");
+string a = Console.ReadLine();
+Console.WriteLine(ThreeDigitNumberHalf(a));
+//Console.WriteLine(a.Length);
