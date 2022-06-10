27ca49242060686c0d7f94751d2e3d313c3efe3c
Nikolay <popov0772@gmail.ru>
2022-06-10 14:30:32 +0500

    added second seminar

---
+++ Seminar2/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+int findMaxPart (int num)
+{
+    if (num >= 10 && <= 99)
+    {
+        if (num / 10 > num % 10)
+        {
+            return num / 10
+        }
+        else
+        {
+            return num % 10
+        }
+    }
+    else {return -1;}
+}
+
+Console.Write("Input to-digit number: ");
+int a = int.Parse(Console.ReadLine());
+
+int result = findMaxPart(a);
+
+ConsoleWriteLine(result);
+
+
+int CutNumber()
+{
+    int num = new Random().Next(100,1000);
+    Console.WriteLine(num)
+    int sot = num /100;
+    int ed = num % 10;
+    return sot *10 + ed
+
+}
+ConsoleWriteLine(CutNumber())
+
+string IsAliquote(int num1, int num2)
+{
+    if (num2 % num1 == 0) return "True";
+    else return ("False, rest num is " +(num2 % num1 ))
+} 
+*/
+
+bool IsAliquot2(int num)
+return (num % 23 == 0 && NotFiniteNumberException % 7 == 0);
+ 
