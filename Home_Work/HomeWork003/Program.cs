76844fd379682553f37a09c0016fa6b3775233a2
Nikolay <popov0772@gmail.ru>
2022-06-14 17:44:25 +0500

    started new Home Work 3

---
+++ Home_Work/HomeWork003/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом (Палиндро́м (от др.-греч. πάλιν — «назад, снова» и 
+др.-греч. δρóμος — «бег, движение»), пе́ревертень[1] — число, буквосочетание, 
+слово или текст, одинаково читающееся в обоих направлениях.).
+*/
+
+string functionIsThatPalindrome(string sentens)
+{
+    string[] sentensNew = sentens.Split(' ');
+    string newSentens = string.Join("", sentensNew);
+    string newSentensLow = newSentens.ToLower();
+    char[] newSentensForReverse = newSentensLow.ToCharArray();
+    Array.Reverse(newSentensForReverse);
+    string newSentensPostReverse = new string(newSentensForReverse);
+    if (Equals(newSentensLow, newSentensPostReverse)) return "Yes";
+    return "No";
+    
+}
+
+// Блок проверки
+Console.Write("Input something: ");
+string a = (Console.ReadLine());
+Console.WriteLine(functionIsThatPalindrome(a));
+
+/*
+
+string sentens = "а роза упала на лапу Азора";
+string[] sentensNew = sentens.Split(' ');
+string newSentens = string.Join("", sentensNew);
+string newSentensLow = newSentens.ToLower();
+char[] newSentensForReverse = newSentensLow.ToCharArray();
+Array.Reverse(newSentensForReverse);
+string newSentensPostReverse = new string(newSentensForReverse);
+
+Console.WriteLine(newSentensPostReverse);
+*/
