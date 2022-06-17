ea33280c09860d9de16aac6da52f63a49178c52f
Nikolay <popov0772@gmail.ru>
2022-06-17 11:12:56 +0500

    started new Home Work 004, ended first task

---
+++ Home_Work/HomeWork004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int MethodPowerOfNumber(int number, int power)
+{
+    int powerOfNumber = 1;
+    for (int i = 1; i <= power; i++)
+        {
+            powerOfNumber *= number;
+        }
+    return powerOfNumber;
+}
+
+Console.Write("Input something: ");
+string b = (Console.ReadLine());
+int number = Convert.ToInt32(b);
+Console.Write("Input something: ");
+string c = (Console.ReadLine());
+int power = Convert.ToInt32(c);
+Console.WriteLine(MethodPowerOfNumber(number, power));
